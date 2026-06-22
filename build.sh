28d97310fd6fe9988854046a09af994dccecffab
Stephen Sorley <stephen.sorley@gmail.com>
2026-06-22 12:33:01 -0400

    shorten file names

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./node_modules/.bin/tsdown give-lively-simple-runtime.ts --dts false --minify --target es2023 --platform browser && mv dist/give-lively-simple-runtime{.js,.min.js}
-./node_modules/.bin/lightningcss --error-recovery --browserslist --bundle --minify give-lively-simple.css -o dist/give-lively-simple.min.css
+./node_modules/.bin/tsdown gl-simple-runtime.ts --dts false --minify --target es2023 --platform browser && mv dist/gl-simple-runtime{.js,.min.js}
+./node_modules/.bin/lightningcss --error-recovery --browserslist --bundle --minify gl-simple.css -o dist/gl-simple.min.css
