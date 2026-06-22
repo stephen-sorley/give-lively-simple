22a90fad282c3f59d8a3727954fd39fde09f65cd
Stephen Sorley <stephen.sorley@gmail.com>
2026-06-22 12:42:43 -0400

    add minimized result files ro repo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./node_modules/.bin/tsdown gl-simple-runtime.ts --dts false --minify --target es2023 --platform browser && mv dist/gl-simple-runtime{.js,.min.js}
+./node_modules/.bin/tsdown gl-simple-runtime.ts gl-simple-builder.ts --dts false --minify --target es2023 --platform browser && mv dist/gl-simple-runtime{.js,.min.js} && mv dist/gl-simple-builder{.js,.min.js}
 ./node_modules/.bin/lightningcss --error-recovery --browserslist --bundle --minify gl-simple.css -o dist/gl-simple.min.css
